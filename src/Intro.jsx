4440d94ee26113e5b61befda1d08ec0618de52cf
Manasi Deore <manasideore202@gmail.com>
2024-12-02 00:45:33 +0530

    Adding the media query for each css file

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -11,41 +11,43 @@ export default function Intro() {
     window.location.href = import.meta.env.VITE_GITHUB_URL; // Replace with your desired URL
   };
   return (
-    <div className="info" id="Intro">
-      <div className="info-left">
-        <p className="info-text">
-          <b>Hi, I'm Manasi Deore.</b>
-          <br />
-          <br />
-          <i>
-            A MERN Stack Developer skilled in creating dynamic, user-friendly
-            web applications. I turn ideas into scalable,seamless digital
-            solutions.
-          </i>
-        </p>
-        <div className="icons">
-          <img
-            src={Linkdin}
-            alt="description"
-            style={{ width: "35px", height: "35px" }}
-            onClick={linkdin}
-          />
+    <div className="content">
+      <div className="info" id="Intro">
+        <div className="info-left">
+          <p className="info-text">
+            <b>Hi, I'm Manasi Deore.</b>
+            <br />
+            <br />
+            <i>
+              A MERN Stack Developer skilled in creating dynamic, user-friendly
+              web applications. I turn ideas into scalable, seamless digital
+              solutions.
+            </i>
+          </p>
+          <div className="icons">
+            <img
+              src={Linkdin}
+              alt="description"
+              style={{ width: "35px", height: "35px" }}
+              onClick={linkdin}
+            />
 
+            <img
+              src={Github}
+              alt="description"
+              style={{ width: "35px", height: "36px" }}
+              onClick={github}
+            />
+          </div>
+        </div>
+
+        <div className="info-right">
           <img
-            src={Github}
+            src={Profile}
             alt="description"
-            style={{ width: "35px", height: "36px" }}
-            onClick={github}
+            style={{ width: "100%", height: "100%" }}
           />
         </div>
-      </div>
-
-      <div className="info-right">
-        <img
-          src={Profile}
-          alt="description"
-          style={{ width: "100%", height: "100%" }}
-        />
       </div>
     </div>
   );
